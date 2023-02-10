09f867873d884b07e952647f8003fe5b85913250
gustavo anjos <gustavoanjos42@gmail.com>
2023-02-10 14:47:49 -0300

    Initial Commit

---
+++ CountriesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountriesApp
+//
+//  Created by Andrya de Melo Quara on 10/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
